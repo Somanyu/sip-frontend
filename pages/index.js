60ef0b1639ac5cc6f51a9d105cd6df071d9446fa
Somanyu <somanyu.03samal@gmail.com>
2022-12-20 20:55:19 +0530

    💄 add: Hero.js component

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
 
 export default function Home() {
   return (
     <>
-      <Navbar/>
+      <Navbar />
+      <Hero />
     </>
   )
 }

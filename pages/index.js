c9fe024da0e70f921415c75ae73977660b829f39
Somanyu <somanyu.03samal@gmail.com>
2022-12-20 20:29:45 +0530

    🧱 Install and set up Flowbite

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-      <h1 className='text-2xl text-red-600 text-center'>Hello world!</h1>
+      <h1 className='text-2xl text-red-600 text-center font-inter'>Hello world!</h1>
+      
     </>
   )
 }

fee11839cdc3e26dce5935397e1611a63a17909d
Somanyu <somanyu.03samal@gmail.com>
2022-12-20 22:14:50 +0530

    💄 add: CTA section

--- components/Hero.js
+++ components/Hero.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import heroImage from "../public/images/heroimage1.png"
+import heroImage from "../public/images/heroimage.png"
 
 export default function Hero() {
     return (
         <>
-            <section class="bg-white dark:bg-gray-900 pb-14">
+            <section class="bg-white dark:bg-gray-900 pt-10">
                 <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
                     <div class="mr-auto place-self-center lg:col-span-7">
                         <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white font-karla">Payments tool for software companies</h1>
@@ -18,8 +18,7 @@ export default function Hero() {
                         </a>
                     </div>
                     <div class="hidden lg:mt-0 lg:col-span-5 lg:flex lg:justify-center lg:content-center">
-                        {/* <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup"> */}
-                        <Image src={heroImage} width="450" height="200" alt="hero image"/>
+                        <Image src={heroImage} width="350" height="200" alt="hero image"/>
                     </div>
                 </div>
             </section>
--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,14 @@ import styles from '../styles/Home.module.css'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import Footer from '../components/Footer'
+import CTA from '../components/CTA'
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <CTA/>
       <Footer />
     </>
   )

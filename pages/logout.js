083b2e988398570b258a436d9d305d1aea122955
Somanyu <somanyu.03samal@gmail.com>
2022-12-21 16:51:21 +0530

    💄 add: logout page

---
+++ pages/logout.js
@@ -0,0 +1,13 @@
+const Logout = () => {
+    return (
+        <>
+            <div class="flex justify-center content-center p-28">
+                <div class="font-inter w-1/2 p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
+                    <span class="font-bold font-inter">Danger alert!</span> Change a few things up and try submitting again.
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Logout;

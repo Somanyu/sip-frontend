a4900b463991d66aa9d854b71627fdd7370dce12
Somanyu <somanyu.03samal@gmail.com>
2022-12-22 12:29:06 +0530

    👔 add: calculate SIP from form inouts

--- pages/calculate.js
+++ pages/calculate.js
@@ -3,27 +3,152 @@ import Navbar from "../components/Navbar";
 import { useState, useEffect } from "react";
 
 const Calculate = () => {
+    // const [principal, setPrincipal] = useState('0');
+    // const [rate, setRate] = useState('0');
+    // const [time, setTime] = useState('0');
+    // const [intervals, setIntervals] = useState('0');
+    // const [sip, setSip] = useState(null);
+    // const [error, setError] = useState(null);
+
+    const [formData, setFormData] = useState({
+        principal: 0,
+        time: 0,
+        rate: 0,
+        intervals: 0,
+    })
+    const [sip, setSip] = useState(null);
+    const [error, setError] = useState(null);
+
+    const handleChange = (event) => {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value,
+        })
+    }
+
+    // const handleChange = (event) => {
+    //     switch (event.target.value) {
+    //         case 'principal':
+    //             setPrincipal(event.target.value)
+    //             break;
+    //         case 'rate':
+    //             setRate(event.target.value)
+    //             break;
+    //         case 'time':
+    //             setTime(event.target.value)
+    //             break;
+    //         case 'intervals':
+    //             setIntervals(event.target.value)
+    //             break;
+
+    //         default:
+    //             break;
+    //     }
+    // }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        async function calculateSIP() {
+            try {
+                const res = await fetch('http://localhost:3001/users/calculate', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(formData),
+                })
+                const data = await res.json();
+                // setSip(data.sip)
+                // setError(null);
+
+                if (res.ok) {
+                    setSip(data);
+                }
+            } catch (error) {
+                setError(error)
+            }
+        }
+        calculateSIP()
+    }
+
+
     return (
         <>
             <Navbar authLink={'/logout'} authType={'Log Out'} />
-            <section class="bg-white dark:bg-gray-900 font-inter p-28">
-                <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-                    <div class="mr-auto place-self-center lg:col-span-7">
-                        <h1 class="font-karla max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Payments tool for software companies</h1>
-                        <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">From checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.</p>
-                        <a href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-                            Get started
-                            <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                        </a>
-                        <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                            Speak to Sales
-                        </a>
-                    </div>
-                    <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-                        {/* <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup"> */}
+
+            <div id="defaultModal" class="bg-white dark:bg-gray-800 overflow-y-auto overflow-x-hidden flex justify-center content-center w-full md:inset-0 md:h-full p-16">
+                <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
+                    {/* <!-- Modal content --> */}
+                    <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
+                        {/* <!-- Modal header --> */}
+                        <div class="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
+                            <h3 class="font-karla text-lg font-semibold text-gray-900 dark:text-white">
+                                Calculate SIP
+                            </h3>
+                        </div>
+                        {/* <!-- Modal body --> */}
+                        <form onSubmit={handleSubmit}>
+                            <div class="grid gap-4 mb-4 sm:grid-cols-2 font-inter">
+                                <div>
+                                    <label for="principal" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Principal</label>
+                                    <input
+                                        type="number"
+                                        onChange={handleChange}
+                                        name="principal"
+                                        id="principal"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        placeholder="Type amount you'll invest"
+                                        required=""
+                                    />
+                                </div>
+                                <div>
+                                    <label for="rate" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Rate</label>
+                                    <input
+                                        type="number"
+                                        onChange={handleChange}
+                                        name="rate"
+                                        id="rate"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        placeholder="Expected rate of return"
+                                        required=""
+                                    />
+                                </div>
+                                <div>
+                                    <label for="time" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Time</label>
+                                    <input
+                                        type="number"
+                                        onChange={handleChange}
+                                        name="time"
+                                        id="time"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        placeholder="Investment duration in months"
+                                        required=""
+                                    />
+                                </div>
+                                {/* <div>
+                                    <label for="intervals" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Intervals</label>
+                                    <input
+                                        type="number"
+                                        onChange={handleChange}
+                                        name="intervals"
+                                        id="intervals"
+                                        step="any"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        placeholder="Compounded rate of return"
+                                        required=""
+                                    />
+                                </div> */}
+
+                            </div>
+                            <button type="submit" class="font-inter text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                <svg class="mr-1 -ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
+                                Calculate
+                            </button>
+                        </form>
                     </div>
                 </div>
-            </section>
+            </div>
+
             <Footer />
 
         </>
